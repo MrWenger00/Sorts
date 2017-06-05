7f2b9861ac5d9ab8be978bb30fb843b97a732c4f
Guilherme Wenger <grw090@gmail.com>
2017-06-05 09:35:42 -0300

    Update
    Update

--- src/HeapSort.java
+++ src/HeapSort.java
@@ -46,47 +46,42 @@ public class HeapSort {
 	public static int[] ordenar(int a[]) {
 		int n = a.length;
 
-		// Build heap (rearrange array)
-		for (int i = n / 2 - 1; i >= 0; i--)
+		for (int i = n / 2 - 1; i >= 0; i--){
 			heapify(a, n, i);
-
-		// One by one extract an element from heap
+		}
 		for (int i = n - 1; i >= 0; i--) {
-			// Move current root to end
+			
 			int temp = a[0];
 			a[0] = a[i];
 			a[i] = temp;
 
-			// call max heapify on the reduced heap
+			
 			heapify(a, i, 0);
 		}
 
 		return a;
 	}
 
-	// To heapify a subtree rooted with node i which is
-	// an index in arr[]. n is size of heap
+	
 	public static void heapify(int a[], int n, int i) {
-		int largest = i; // Initialize largest as root
-		int l = 2 * i + 1; // left = 2*i + 1
-		int r = 2 * i + 2; // right = 2*i + 2
+		int largura = i; 
+		int l = 2 * i + 1; 
+		int r = 2 * i + 2; 
 
-		// If left child is larger than root
-		if (l < n && a[l] > a[largest])
+		if (l < n && a[l] > a[largura]){
 			largest = l;
-
-		// If right child is larger than largest so far
-		if (r < n && a[r] > a[largest])
+		}
+		if (r < n && a[r] > a[largura]){
 			largest = r;
+		}
+		
+		if (largura != i) {
+			int aux = a[i];
+			a[i] = a[largura];
+			a[largura] = aux;
 
-		// If largest is not root
-		if (largest != i) {
-			int swap = a[i];
-			a[i] = a[largest];
-			a[largest] = swap;
-
-			// Recursively heapify the affected sub-tree
-			heapify(a, n, largest);
+			
+			heapify(a, n, largura);
 		}
 	}
 }
